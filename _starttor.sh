a566471174a47b47140379aa6c3b28b870e020dc
fckpgn <31369251+fckpgn@users.noreply.github.com>
2017-09-05 21:01:16 +0300

    fix iptables command and bash

--- _starttor.sh
+++ _starttor.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 \cp /overlay/mnt/iplists/_torconfig /etc/torrc
-/etc/init.d/tor start
+/etc/init.d/tor restart
 iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 9053
 iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 9053
 
 while read listfile; do 
 	while read range; do
-		iptables -t nat -A PREROUTING -p tcp --dst-range $range -j REDIRECT --to-ports 9040
+		iptables -t nat -A PREROUTING -p tcp -m iprange --dst-range $range -j REDIRECT --to-ports 9040
 	done < /overlay/mnt/iplists/$listfile
 done < /overlay/mnt/iplists/_list.txt
