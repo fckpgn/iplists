9863cb2da23c19b472be60ece0f3d6eb493a7bce
fckpgn <31369251+fckpgn@users.noreply.github.com>
2017-09-05 21:30:32 +0300

    some fixes

--- _starttor.sh
+++ _starttor.sh
@@ -4,8 +4,11 @@
 iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 9053
 iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 9053
 
+
 while read listfile; do 
-	while read range; do
-		iptables -t nat -A PREROUTING -p tcp -d $range -j REDIRECT --to-ports 9040
-	done < /overlay/mnt/iplists/$listfile
+	cat /overlay/mnt/iplists/$listfile >> /overlay/mnt/iplists/ip.list
 done < /overlay/mnt/iplists/_list.txt
+
+while read range; do
+	iptables -t nat -A PREROUTING -p tcp -d $range -j REDIRECT --to-ports 9040
+done < /overlay/mnt/iplists/ip.list
