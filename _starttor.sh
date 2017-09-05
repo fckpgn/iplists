d5684d1c4f9cf12d868dce4f2fb2a49342c0fb6e
fckpgn <31369251+fckpgn@users.noreply.github.com>
2017-09-05 20:50:49 +0300

    fix path for overlay

--- _starttor.sh
+++ _starttor.sh
@@ -1,7 +1,10 @@
-\cp /mnt/iplists/_torconfig /etc/torrc
+\cp /overlay/mnt/iplists/_torconfig /etc/torrc
 /etc/init.d/tor start
+iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 9053
+iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 9053
+
 while read listfile; do 
 	while read range; do
-		iptables -A INPUT -p tcp -m iprange --dst-range $range -j ACCEPT
-	done < /mnt/iplists/$listfile
-done < /mnt/iplists/_list.txt
+		iptables -t nat -A PREROUTING -p tcp --dst-range $range -j REDIRECT --to-ports 9040
+	done < /overlay/mnt/iplists/$listfile
+done < /overlay/mnt/iplists/_list.txt
