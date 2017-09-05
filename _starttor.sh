4e808d4260ec18204478a9efcf81ba8a043ecdb4
fckpgn <31369251+fckpgn@users.noreply.github.com>
2017-09-05 22:14:35 +0300

    disable DNS via Tor

--- _starttor.sh
+++ _starttor.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 \cp /overlay/mnt/iplists/_torconfig /etc/torrc
+\cp /overlay/mnt/iplists/_torconfig /etc/tor/torrc
+chown tor:root /etc/torrc
+chown tor:root /etc/tor/torrc
 /etc/init.d/tor restart
-iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 9053
-iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 9053
+#iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 9053
+#iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 9053
 
 
 while read listfile; do 
